e53e290a0642c16ad167993c4ef69dad9d1b93b1
June Jung <costalgia@gmail.com>
2020-06-13 13:46:28 -0700

    applying sass style

--- src/app.js
+++ src/app.js
@@ -2,5 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import DecisionApp from './components/DecisionApp';
-
+import 'normalize.css/normalize.css';
+import './styles/styles.scss';
 ReactDOM.render(<DecisionApp />, document.getElementById('app'));
